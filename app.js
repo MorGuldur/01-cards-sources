7aa942f0a8bb9baecb0bb5059ad1fb9634bf0449
Mor Guldur <marcus91@bk.ru>
2021-09-29 19:00:49 +0300

    Add project

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-function slidesPlugin(activeSlide = 0) {
+function slidesPlugin(activeSlide = 2) {
 	const slides = document.querySelectorAll('.slide');
 
-	slides[activeSlide].classList.add('active')
+	slides[activeSlide].classList.add('active');
 
 	for (const slide of slides) {
 		slide.addEventListener('click', () => {
-			clearActiveClasses()
+			clearActiveClasses();
 
-			slide.classList.add('active')
-		})
-	};
+			slide.classList.add('active');
+		});
+	}
 
 	function clearActiveClasses() {
 		slides.forEach((slide) => {
-			slide.classList.remove('active')
+			slide.classList.remove('active');
 		});
-	};
-};
+	}
+}
 
-slidesPlugin(2);
+slidesPlugin(0);
